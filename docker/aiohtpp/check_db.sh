fa9795691436a3d889d46a53581b48cd1040333e
Guilherme de Almeida Costa <costaguila@gmail.com>
2020-12-05 19:42:12 -0300

    Ajustes no script de conexão e criação do banco

--- docker/aiohtpp/check_db.sh
+++ docker/aiohtpp/check_db.sh
@@ -2,7 +2,7 @@
 
 until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$1" -U $POSTGRES_USER -d $POSTGRES_DB -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
+  sleep 5
 done
 
 >&2 echo "Postgres is up - executing command"
